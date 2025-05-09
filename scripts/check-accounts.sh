a8fe484cb0a98adad95265025460f52ba4451a50
Jessy Ssebuliba <jessiessebuliba@gmail.com>
2025-05-09 09:43:30 +0300

    fix syntax
    Signed-off-by: Jessy Ssebuliba <jessiessebuliba@gmail.com>

--- scripts/check-accounts.sh
+++ scripts/check-accounts.sh
@@ -3,29 +3,29 @@
 set -e
 
 # Validate ECDSA Outputs
-if [ -z "${{ steps.solo.outputs.ecdsaAccountId }}" ]; then
+if [ -z "$ECDSA_ACCOUNT_ID" ]; then
     echo "❌ Error: ECDSA accountId is missing!"
     exit 1
 fi
-if [ -z "${{ steps.solo.outputs.ecdsaPrivateKey }}" ]; then
+if [ -z "$ECDSA_PRIVATE_KEY" ]; then
     echo "❌ Error: ECDSA privateKey is missing!"
     exit 1
 fi
-if [ -z "${{ steps.solo.outputs.ecdsaPublicKey }}" ]; then
+if [ -z "$ECDSA_PUBLIC_KEY" ]; then
     echo "❌ Error: ECDSA publicKey is missing!"
     exit 1
 fi
 
 # Validate ED25519 Outputs
-if [ -z "${{ steps.solo.outputs.ed25519AccountId }}" ]; then
+if [ -z "$ED25519_ACCOUNT_ID" ]; then
     echo "❌ Error: ED25519 accountId is missing!"
     exit 1
 fi
-if [ -z "${{ steps.solo.outputs.ed25519PrivateKey }}" ]; then
+if [ -z "$ED25519_PRIVATE_KEY" ]; then
     echo "❌ Error: ED25519 privateKey is missing!"
     exit 1
 fi
-if [ -z "${{ steps.solo.outputs.ed25519PublicKey }}" ]; then
+if [ -z "$ED25519_PUBLIC_KEY" ]; then
     echo "❌ Error: ED25519 publicKey is missing!"
     exit 1
 fi
